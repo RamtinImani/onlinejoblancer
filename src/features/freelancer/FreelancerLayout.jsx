6a363fa1140a37782fe7e554e4459b0ff98fb124
Ramtin Imani <ramtinimani.dev@gmail.com>
2025-10-30 18:10:49 +0330

    Add routes, pages, and the FreelancerLayout component for the Freelancer layout

---
+++ src/features/freelancer/FreelancerLayout.jsx
@@ -0,0 +1,29 @@
+import { HiCollection, HiHome, HiOutlineViewGrid } from "react-icons/hi";
+import AppLayout from "../../ui/AppLayout";
+import Sidebar from "../../ui/Sidebar";
+import CustomNavLink from "../../ui/CustomNavLink";
+
+function FreelancerLayout() {
+  return (
+    <AppLayout>
+      <Sidebar>
+        <CustomNavLink to="dashboard">
+          <HiHome />
+          <span>داشبورد</span>
+        </CustomNavLink>
+
+        <CustomNavLink to="projects">
+          <HiOutlineViewGrid />
+          <span>پروژه ها</span>
+        </CustomNavLink>
+
+        <CustomNavLink to="proposals">
+          <HiCollection />
+          <span>درخواست ها</span>
+        </CustomNavLink>
+      </Sidebar>
+    </AppLayout>
+  );
+}
+
+export default FreelancerLayout;
