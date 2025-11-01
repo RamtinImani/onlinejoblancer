ddfdeb321613c1b387e074f8c82be42631b854a7
Ramtin Imani <ramtinimani.dev@gmail.com>
2025-11-01 19:45:21 +0330

    Add FilterDropDown component for filter projects based on sorting options

--- src/features/freelancer/projects/ProjectsHeader.jsx
+++ src/features/freelancer/projects/ProjectsHeader.jsx
@@ -1,13 +1,26 @@
 import useCategories from "../../../hooks/useCategories";
 import FilterDropDown from "../../../ui/FilterDropDown";
 
+const sortOptions = [
+  {
+    label: "جدید ترین",
+    value: "latest",
+  },
+  {
+    label: "قدیمی ترین",
+    value: "earliest",
+  },
+];
+
 function ProjectsHeader() {
   const { transformedCategories } = useCategories();
 
   return (
     <div className="flex items-center justify-between text-secondary-700 mb-8">
       <h1 className="font-black text-2xl">لیست پروژه ها</h1>
-      <div>
+      <div className="flex items-center gap-x-4">
+        <FilterDropDown filterField="sort" options={sortOptions} />
+
         <FilterDropDown
           filterField="category"
           options={[
