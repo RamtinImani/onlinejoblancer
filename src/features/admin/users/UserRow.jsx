6507abda0ef7273e23b20bdc0ab484542005fc66
Ramtin Imani <ramtinimani.dev@gmail.com>
2025-11-29 09:05:58 +0330

    Add UsersTable component for users data

---
+++ src/features/admin/users/UserRow.jsx
@@ -0,0 +1,51 @@
+import { HiPencil } from "react-icons/hi";
+import Table from "../../../ui/Table";
+import { toPersianNumbers } from "../../../utils/toPersianNumber";
+import truncateText from "../../../utils/truncateText";
+import Modal from "../../../ui/Modal";
+import { useState } from "react";
+
+const statusStyle = [
+  {
+    label: "رد شده",
+    className: "badge--danger",
+  },
+  {
+    label: "در انتظار تایید",
+    className: "badge--secondary",
+  },
+  {
+    label: "تایید شده",
+    className: "badge--success",
+  },
+];
+
+function UserRow({ user, index }) {
+  const { name, email, phoneNumber, role, status } = user;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Table.Row>
+      <td>{index + 1}</td>
+      <td>{name}</td>
+      <td>{truncateText(email, 30)}</td>
+      <td>{toPersianNumbers(phoneNumber)}</td>
+      <td>{role}</td>
+      <td>
+        <span className={`badge ${statusStyle[status].className}`}>
+          {statusStyle[status].label}
+        </span>
+      </td>
+      <td>
+        <button onClick={() => setOpen(true)}>
+          <HiPencil className="size-5 text-primary-900" />
+        </button>
+        <Modal title="تغییر وضعیت کاربر" open={open} onClose={() => setOpen(false)}>
+          change user status component
+        </Modal>
+      </td>
+    </Table.Row>
+  );
+}
+
+export default UserRow;
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,12 @@
+import UsersTable from "../features/admin/users/UsersTable";
+
 function Users() {
-  return <div>Users</div>;
+  return (
+    <div>
+      <h1 className="font-black text-secondary-700 text-2xl mb-8">کاربران شما</h1>
+      <UsersTable />
+    </div>
+  );
 }
 
 export default Users;
